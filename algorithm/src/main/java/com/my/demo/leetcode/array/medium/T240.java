75428fb2b4a66a657d651ee00c63e43a74b70f95
ffdeng <595188240@qq.com>
2021-10-25 10:15:05 +0800

    T240 矩阵中快速查找

---
+++ algorithm/src/main/java/com/my/demo/leetcode/array/medium/T240.java
@@ -0,0 +1,32 @@
+package com.my.demo.leetcode.array.medium;
+
+/**
+ * @author ffdeng2
+ */
+public class T240 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 50;
+        System.out.println(searchMatrix(matrix, target));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int x = 0;
+        int y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+
+}
